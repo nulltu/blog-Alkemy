bf87096c95c7153f76b842fcb79df6e86c3a930d
nulltu <rusbent.matta@gmail.com>
2020-12-16 20:22:17 -0300

    config environment variables

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const routes = require('./routes/api');
 require('dotenv').config();
 
+const PORTDEFAULT = 8082;
+
 const app = express();
 
 require('./config/db');
@@ -12,7 +14,7 @@ app.use(express.json());
 
 app.use('/api', routes);
 
-const PORT = process.env.PORTSERVER;
+const PORT = process.env.PORTSERVER || PORTDEFAULT;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
